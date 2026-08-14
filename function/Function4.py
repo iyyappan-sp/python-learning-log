c4063b781f221477af49a2bf46940c85d2c137a5
iyyappan-sp <data.iyyappan@gmail.com>
2026-08-14 21:36:03 +0530

    feat : python functions

---
+++ function/Function4.py
@@ -0,0 +1,23 @@
+def add(a,b):
+    print("A=",a)
+    print("B=",b)
+add(12,34)
+
+
+
+
+
+
+def add1(a,b):
+    print("A=",a)
+    print("B=",b)
+add1(b=12,a=34)
+
+
+
+def add2(a = 1, b = 2):
+    print("A=",a)
+    print("B=",b)
+
+add2()
+add2(10,20)
