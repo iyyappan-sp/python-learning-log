52555659118febad83e5d7b7836f566599843590
iyyappan-sp <data.iyyappan@gmail.com>
2026-08-18 21:42:10 +0530

    feat : python functions

---
+++ oop/Ad example.py
@@ -0,0 +1,12 @@
+
+
+class Student:
+    def __init__(self):     #constructor (Special type of Method)
+        self.name = "S.I.R...."
+        print(self.name)    #
+
+    def printname(self):    #Normal Methods
+        print(self.name)
+
+s = Student()   #Object Creation
+#s.printname()  #Method Calling
