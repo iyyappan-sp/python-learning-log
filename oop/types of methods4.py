1d8624bc875ecab24438279e23286481ef90cf97
iyyappan-sp <data.iyyappan@gmail.com>
2026-08-19 21:08:06 +0530

    feat : python oops concepts

---
+++ oop/types of methods4.py
@@ -0,0 +1,19 @@
+
+class Calculator:
+
+    @staticmethod
+    def add():
+        a = 10
+        b = 20
+        print(a+b)
+
+    @staticmethod
+    def sub():
+        a = 50
+        b = 30
+        print(a-b)
+        
+
+Calculator.add()
+c = Calculator()
+c.sub()
