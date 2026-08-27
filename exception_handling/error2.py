6a9a87759e9a91f096f8a4f8e1e8ce65010839e6
iyyappan-sp <data.iyyappan@gmail.com>
2026-08-27 19:20:50 +0530

    feat : python exception handling

---
+++ exception_handling/error2.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+#print("Program Begins")
+#print(10/0)
+#print("Program Ends")
+
+
+
+
+
+
+
+print("Program Begins")
+try:
+    print(10/0)
+except ZeroDivisionError:
+    print(10/2)
+print("Program Ends")
